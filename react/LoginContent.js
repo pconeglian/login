6e3f154c591a447769c6199cef4607c79b49d156
Lucas Bebber <lucasbbebber@gmail.com>
2020-02-03 18:03:37 -0300

    Move schema to entry point LoginContent component

--- react/LoginContent.js
+++ react/LoginContent.js
@@ -13,4 +13,35 @@ const LoginContentWrapper = props => {
   )
 }
 
+LoginContentWrapper.getSchema = () => ({
+  title: 'admin/editor.loginPage.title',
+  ...LoginSchema,
+  properties: {
+    ...LoginSchema.properties,
+    isInitialScreenOptionOnly: {
+      title: 'admin/editor.login.isInitialScreenOptionOnly.title',
+      type: 'boolean',
+      default: true,
+      isLayout: true,
+    },
+    defaultOption: {
+      title: 'admin/editor.login.defaultOption.title',
+      type: 'number',
+      default: 0,
+      isLayout: true,
+      enum: [0, 1],
+      enumNames: [
+        'admin/editor.login.defaultOption.token',
+        'admin/editor.login.defaultOption.emailAndPassword',
+      ],
+      widget: {
+        'ui:widget': 'radio',
+        'ui:options': {
+          inline: true,
+        },
+      },
+    },
+  },
+})
+
 export default LoginContentWrapper
--- react/components/LoginContent.js
+++ react/components/LoginContent.js
@@ -419,35 +419,4 @@ const content = withSession()(
   )(LoginContent)
 )
 
-content.getSchema = () => ({
-  title: 'admin/editor.loginPage.title',
-  ...LoginSchema,
-  properties: {
-    ...LoginSchema.properties,
-    isInitialScreenOptionOnly: {
-      title: 'admin/editor.login.isInitialScreenOptionOnly.title',
-      type: 'boolean',
-      default: true,
-      isLayout: true,
-    },
-    defaultOption: {
-      title: 'admin/editor.login.defaultOption.title',
-      type: 'number',
-      default: 0,
-      isLayout: true,
-      enum: [0, 1],
-      enumNames: [
-        'admin/editor.login.defaultOption.token',
-        'admin/editor.login.defaultOption.emailAndPassword',
-      ],
-      widget: {
-        'ui:widget': 'radio',
-        'ui:options': {
-          inline: true,
-        },
-      },
-    },
-  },
-})
-
 export default withRuntimeContext(content)
