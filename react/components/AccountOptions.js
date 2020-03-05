108f127d9b55dadf8c8b90c02dc39122f581e4c1
Rafael Rubim <rafael.rubim.cabral@gmail.com>
2020-03-05 11:51:33 -0300

    Make AccountOptions a function component

--- react/components/AccountOptions.js
+++ react/components/AccountOptions.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react'
+import React, { useMemo } from 'react'
 import PropTypes from 'prop-types'
 import { injectIntl, intlShape } from 'react-intl'
 import { Link } from 'vtex.render-runtime'
-
 import { Button } from 'vtex.styleguide'
 import { AuthServiceLazy } from 'vtex.react-vtexid'
 
@@ -10,78 +9,73 @@ import { translate } from '../utils/translate'
 import styles from '../styles.css'
 
 // Component that shows account options to the user.
-class AccountOptions extends Component {
-  static propTypes = {
-    /** Intl object*/
-    intl: intlShape,
-    optionLinks: PropTypes.array,
-  }
+const AccountOptions = ({ intl, optionLinks }) => {
+  const hasOptionLinks = useMemo(() => optionLinks && optionLinks.length > 0, [
+    optionLinks,
+  ])
 
-  render() {
-    const { intl, optionLinks } = this.props
-
-    const hasOptionLinks = optionLinks && optionLinks.length > 0
-
-    return (
-      <div className={`${styles.accountOptions} items-center w-100`}>
-        <div className="ma4 min-h-2 b--muted-4">
-          {optionLinks && optionLinks.length > 0 ?
-            <>
-              <div className="t-small b pb4">
+  return (
+    <div className={`${styles.accountOptions} items-center w-100`}>
+      <div className="ma4 min-h-2 b--muted-4">
+        {optionLinks && optionLinks.length > 0 ? (
+          <>
+            <div className="t-small b pb4">
+              {translate('store/login.myAccount', intl)}
+            </div>
+            {optionLinks.map(({ label, path }) => (
+              <a
+                className="db no-underline t-small c-muted-1 hover-c-action-primary pb3"
+                href={new URL(path || '/', window.location.href).href}
+              >
+                {label}
+              </a>
+            ))}
+          </>
+        ) : (
+          <Link page="store.account">
+            <button
+              className={`${styles.button} bw1 ba ttu br2 t-action--small v-mid relative pv3 ph5 t-heading-5 bg-base b--transparent c-action-primary  hover-c-action-primary pointer`}
+              closeonclick=""
+            >
+              <span className="t-action--small">
                 {translate('store/login.myAccount', intl)}
-              </div>
-              {optionLinks.map(({ label, path }) =>
-                <a
-                  className={'db no-underline t-small c-muted-1 hover-c-action-primary pb3'}
-                  href={new URL(path || '/', window.location.href).href}
-                >
-                  {label}
-                </a>
-              )}
-            </>
-          :
-            <Link page={'store.account'}>
-              <button
-                className={`${styles.button} bw1 ba ttu br2 t-action--small v-mid relative pv3 ph5 t-heading-5 bg-base b--transparent c-action-primary  hover-c-action-primary pointer`}
-                closeonclick=""
-              >
-                <span className="t-action--small">
-                  {translate('store/login.myAccount', intl)}
-                </span>
-              </button>
-            </Link>
-          }
-        </div>
-        <hr className="mv2 o-30" />
-        <div className="ma4 min-h-2 b--muted-4">
-          <AuthServiceLazy.RedirectLogout returnUrl="/">
-            {({ action: logout }) => {
-              if (hasOptionLinks) {
-                return (
-                  <button
-                    className="t-small bn pa0 c-muted-1 hover-c-danger pointer"
-                    onClick={logout}
-                  >
-                    {translate('store/login.logoutLabel', intl)}
-                  </button>
-                )
-              }
+              </span>
+            </button>
+          </Link>
+        )}
+      </div>
+      <hr className="mv2 o-30" />
+      <div className="ma4 min-h-2 b--muted-4">
+        <AuthServiceLazy.RedirectLogout returnUrl="/">
+          {({ action: logout }) => {
+            if (hasOptionLinks) {
               return (
-              <Button
-                variation="tertiary"
-                size="small"
-                onClick={logout}
-              >
+                <button
+                  className="t-small bn pa0 c-muted-1 hover-c-danger pointer"
+                  onClick={logout}
+                >
+                  {translate('store/login.logoutLabel', intl)}
+                </button>
+              )
+            }
+            return (
+              <Button variation="tertiary" size="small" onClick={logout}>
                 <span className="t-action--small">
                   {translate('store/login.logoutLabel', intl)}
                 </span>
               </Button>
-            )}}
-          </AuthServiceLazy.RedirectLogout>
-        </div>
+            )
+          }}
+        </AuthServiceLazy.RedirectLogout>
       </div>
-    )
-  }
+    </div>
+  )
+}
+
+AccountOptions.propTypes = {
+  /** Intl object */
+  intl: intlShape,
+  optionLinks: PropTypes.array,
 }
 
 export default injectIntl(AccountOptions)
